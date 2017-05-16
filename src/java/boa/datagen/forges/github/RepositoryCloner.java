3fe71dbc5b1465221f93e4e0c851ad3bc6438e3c
robert2 <robert2@atan-116a-04.cs.iastate.edu>
2017-05-16 11:36:45 -0500

    added a method  to extract clone url from files in repoName directory.

--- src/java/boa/datagen/forges/github/RepositoryCloner.java
+++ src/java/boa/datagen/forges/github/RepositoryCloner.java
@@ -1,11 +1,16 @@
 package boa.datagen.forges.github;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Scanner;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 
 
@@ -17,9 +22,60 @@ import org.eclipse.jgit.api.errors.TransportException;
 public class RepositoryCloner {
 
     private static  String REMOTE_URL = "";
+    private HttpURLConnection connection = null;
 
+    
+    public static void clone(String input, String output) throws InvalidRemoteException, TransportException, IOException, GitAPIException{
+    File in;
+   // TokenList tokens = new TokenList(tokenFile);
+   // Token tok = null;
+   // File out = new File(output);
+    String urlHeader = "https://github.com/";
+    String urlFooter = ".git";
+    String outFilePath= output;
+    String name = "";
+    String nameAndValue[];
+    int pageNumber;
+    Scanner sc = null;
+    File dir = new File(input);
+    if (!dir.exists())
+        dir.mkdirs();
+    File[] files = dir.listFiles();
+    
+    
+    for(int i = 0; i < files.length ; i++){
+    	System.out.println("proccesing file number " + i );
+    	in = files[i];
+    	try {		
+    		sc = new Scanner(in);
+    	} catch (FileNotFoundException e) {		
+    		// TODO Auto-generated catch block		
+    		System.out.println(input +" file not found");
+    		e.printStackTrace();
+    	}
+    	sc.useDelimiter(",");
+    	while(sc.hasNext()){
+    		name = sc.next();
+    	//	System.out.println(name);
+    		nameAndValue = name.split(":");
+    		String keyw = nameAndValue[0].substring(1, nameAndValue[0].length()-1) ;//name.substring(1, 10);
+    	//	System.out.println("keyword: " + keyw);
+    		if(keyw.equals("full_name") ){
+    			//sc.next();
+    			name = nameAndValue[1].substring(1, nameAndValue[1].length() - 1);
+    			outFilePath = output + "/" + name ; 
+    			System.out.println("url: "+ urlHeader + name + urlFooter);
+    			//tok = tokens.getNextAuthenticToken(urlHeader + name + urlFooter);
+    			 String[] args = { urlHeader + name + urlFooter, outFilePath};
+    			 clone(args);
+    		}
+    	} 
+    	System.out.println("finished file " + i );
+    }
+    } 
+    
     public static void clone(String[] args) throws IOException, InvalidRemoteException, TransportException, GitAPIException {
-        // prepare a new folder for the cloned repository
+    	// prepare a new folder for the cloned repository
     	String localpaths=args[1];
     	String url=args[0];
     	REMOTE_URL=url;
@@ -46,7 +102,13 @@ public class RepositoryCloner {
     }
     
     public static void main(String[] args) throws IOException, InvalidRemoteException, TransportException, GitAPIException {
-        // prepare a new folder for the cloned repository
+      String input = args[0];
+      String output= args[1];
+      clone(input, output);
+    	
+    	
+    	/*
+    	// prepare a new folder for the cloned repository
     	String localpath=args[1];
     	String url=args[0];
     	REMOTE_URL=url;
@@ -70,5 +132,7 @@ public class RepositoryCloner {
 			if (result != null && result.getRepository() != null)
 				result.getRepository().close();
 		}
+		*/
     }
+    
 }
